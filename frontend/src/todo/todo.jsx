cc91e887241af3e0c5a49d8de642cee723d38ad4
Henrique Artur <henriqueacg@gmail.com>
2020-08-15 13:50:25 -0300

    add handles to inputs

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -4,11 +4,29 @@ import ToDoForm from './todoForm'
 import ToDoList from './todoList'
 
 export default class ToDo extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { description: '', list: [] }
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+
+    handleChange(e) {
+        this.setState({...this.state, description: e.target.value})
+    }
+
+    handleAdd() {
+        console.log(this.state.description)
+    }
+    
     render() {
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro' />
-                <ToDoForm />
+                <ToDoForm description={ this.state.description }
+                    handleChange={this.handleChange}
+                    handleAdd={this.handleAdd} />
                 <ToDoList />
             </div>
         )
